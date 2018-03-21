bbf2087321a1b996a7c23165029155b4289ff466
Vinicius Pietscher <vini.914@gmail.com>
2018-03-21 13:07:21 -0300

    add more validation methods for slug

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,7 +2,7 @@ class Channel < ApplicationRecord
   has_many    :messages, as: :messageable, dependent: :destroy
   belongs_to  :user
   belongs_to  :team
-  validates   :slug, presence: true
+  validates   :slug, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates   :team, presence: true
   validates   :user, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ApplicationRecord
   belongs_to  :user
   has_many    :talks
   has_many    :channels
-  validates   :slug, presence: true
+  validates   :slug, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates   :user, presence: true
 end
