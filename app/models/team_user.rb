661194907538bf7d5b97bf39883f2260d762f0a7
Vinicius Pietscher <vini.914@gmail.com>
2018-03-21 13:16:42 -0300

    add scope to model TeamUser

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -3,4 +3,5 @@ class TeamUser < ApplicationRecord
   belongs_to :team
   validates  :team, presence: true
   validates  :user, presence: true
+  validates  :user_id, uniqueness: true, scope: :team_id
 end
